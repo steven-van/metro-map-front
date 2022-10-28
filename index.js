661a05294342fd1c9e4e4e9fc5ee6ce96ab2be4d
steven-van <64859587+steven-van@users.noreply.github.com>
2022-10-28 22:25:20 +0200

    feat: place points

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ const fetchStations = async () => {
   }
 };
 
+const fetchPoints = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/points`);
+    points = response.data;
+    console.log(points);
+    return points;
+  } catch (errors) {
+    console.log(errors);
+  }
+};
+
 const clearSelection = () => {
   let startStation = document.getElementById("start");
   let endStation = document.getElementById("end");
@@ -33,19 +44,25 @@ const clearSelection = () => {
   endStation.selectedIndex = 0;
 };
 
-function addPoints(stations) {
+const addPoints = async () => {
+  let stations = await fetchPoints();
   const svgNS = "http://www.w3.org/svg";
   const container = document.getElementById("stations");
-  for (const nomStation of Object.keys(stations)) {
-    const point = document.createFlementNS(svglis, "circle");
-    point.setAttributeNs(null, "cx", stations[nomStation].x);
-    point.setAttributeNs(null, "су", stations[nomStation].y);
-    point.setAttributeNs(null, "r", 5);
-    //point. dataset. name = nomStation;
+  for (let i = 0; i < stations.length; i++) {
+    const point = document.createElementNS(svgNS, "circle");
+    point.setAttributeNS(null, "cx", stations[i].x);
+    point.setAttributeNS(null, "су", stations[i].y);
+    point.setAttributeNS(null, "r", 3);
+    point.setAttributeNS(null, "style", "cursor:pointer;");
+    point.setAttributeNS(null, "stroke", "black");
+    point.setAttributeNS(null, "stroke-width", 1);
+    point.setAttributeNS(null, "fill", "white");
+
     container.appendChild(point);
   }
-}
+};
 
 window.addEventListener("load", () => {
   fetchStations();
+  addPoints();
 });
