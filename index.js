0fc3c5377764fa35ea3115f4403089a1ddafa6a4
steven-van <64859587+steven-van@users.noreply.github.com>
2023-02-14 15:53:28 +0100

    feat: mockup rendered content without data

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const fetchStations = async () => {
 const fetchPoints = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/points`);
-    points = response.data;
+    const points = response.data;
     return points;
   } catch (errors) {
     console.log(errors);
@@ -53,20 +53,56 @@ const addPoints = async () => {
     .join("");
 };
 
+const fetchAcpm = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/acpm`);
+    const acpm = response.data;
+    return acpm;
+  } catch (errors) {
+    console.log(errors);
+  }
+};
+
+const toggleAcpm = () => {
+  const metroMap = document.getElementById("metro-map");
+  const svgPoints = document.getElementById("points");
+  if (metroMap.src.includes("imgs/metrof_r.png")) {
+    metroMap.src = "imgs/acpm.png";
+    svgPoints.style.visibility = "hidden";
+  } else {
+    metroMap.src = "imgs/metrof_r.png";
+    svgPoints.style.visibility = "visible";
+  }
+};
+
 const getItinerary = async () => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/itinerary/${startSelectedValue}/${endSelectedValue}`
-    );
-
     const startSelect = document.getElementById("start");
     const startSelectedValue =
       startSelect.options[startSelect.selectedIndex].value;
     const endSelect = document.getElementById("start");
     const endSelectedValue = endSelect.options[endSelect.selectedIndex].value;
 
+    const response = await axios.get(
+      `${BASE_URL}/itinerary/${startSelectedValue}/${endSelectedValue}`
+    );
+
     const itinerary = response.data;
     console.log(itinerary);
+
+    const lines = new Set();
+    for (let i = 0; i < itinerary.sommets.length; i++) {
+      lines.add(itinerary.sommets[i].numLigne);
+    }
+
+    const globalInfos = document.getElementById("global-infos");
+    globalInfos.innerHTML =
+      `<p class="text-gray-500">Estimated time</p>` +
+      `<div class="flex justify-between">
+    <p class="text-gray-300 font-bold">${itinerary.temps}</p>
+    <p class="text-gray-300 font-bold">${lines.length} line changes</p>
+    <p class="text-gray-300 font-bold">${itinerary.sommets.length}</p>
+  </div>`;
   } catch (errors) {
     console.log(errors);
   }
@@ -77,4 +113,5 @@ window.addEventListener("load", () => {
   addPoints();
   document.getElementById("go").addEventListener("click", getItinerary);
   document.getElementById("clear").addEventListener("click", clearSelection);
+  document.getElementById("acpm").addEventListener("click", toggleAcpm);
 });
